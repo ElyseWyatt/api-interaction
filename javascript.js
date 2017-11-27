01d89983ec222debcb6aff8a7bb134211f537a77
Elyse Wyatt <hi.elyse.wyatt@gmail.com>
2017-11-27 21:56:25 +1300

    Edits.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,29 @@
-window.onload = function (){
+
+
+window.onload = function() {
+  var httpRequest;
   document.getElementById("ajaxButton").addEventListener('click', makeRequest);
+
+  function makeRequest() {
+    httpRequest = new XMLHttpRequest();
+
+    if (!httpRequest) {
+      alert('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('GET', 'https://eda-te-reo.herokuapp.com/api/proverbs' true);
+    httpRequest.send();
+  }
+
+  function alertContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        alert(httpRequest.responseText);
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+  }
 }
